daa37aa62a2ca26ffa42c254bbdd5e4fdb608c86
Alexandru Smeu <alexandru.smeu@pss.ro>
2021-03-22 23:21:50 +0200

    Added test to cover lib2_sum function

--- lib2/test/test1.cpp
+++ lib2/test/test1.cpp
@@ -2,8 +2,10 @@
     #include <Windows.h>
 #endif
 #include "catch2/catch.hpp"
+#include "lib2.h"
 
 TEST_CASE("lib1 test1", "[test1]")
 {
-    REQUIRE(1 == 1);
+    auto sum = lib2_sum(4, 5);
+    REQUIRE(9 == sum);
 }
